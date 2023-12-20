a718b3dc204902cffb1f7972a65540b5ac5d36cd
VanacV <vanya_ev@mail.ru>
2023-12-20 12:34:43 +0300

    Add Logical

--- GLUT/Source.cpp
+++ GLUT/Source.cpp
@@ -9,14 +9,14 @@
 #define LEFT 3
 #define RIGHT 4
 
-// Status Variables
+
 GLint   lvl = 1;
 GLint   points = 0;
 GLint   size = 0;
 GLbyte  gameOver = true;
 GLbyte  EnableLight = true;
 
-// Snake Variables
+
 GLint   bodyPos[2][100] = { {} };
 GLint   _x = 5;
 GLint   _z = 10;
@@ -24,11 +24,11 @@ GLint   _oldX[2] = {};
 GLint   _oldZ[2] = {};
 GLbyte  direction = 0;
 
-// Food Variables
+
 GLint   _bx = 0;
 GLint   _bz = 0;
 
-// Screen variables
+
 GLint   _w = 800;
 GLint   _h = 550;
 GLint   _Giw = 0;
@@ -36,45 +36,46 @@ GLint   _Gih = 0;
 GLint   _Gfw = 150;
 GLint   _Gfh = 150;
 
-//Variables for the Camera Angle
+
 static GLfloat view_rotx = 45.0F;
 static GLfloat view_roty = 0.0F;
 static GLfloat view_rotz = 0.0F;
 
 static GLfloat headRotation = 90.0F;
 static GLfloat zoom = -300.0f;
-//Configure the lightning
+
+
 void initLight()
 {
-    //Add ambient light
+    
     GLfloat ambientColor[] = { 0.3f, 0.4f, 0.8f, 1.0f };
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
-    //Add positioned light
+   
     GLfloat lightColor0[] = { 0.5f, 0.5f, 0.5f, 1.0f };
     GLfloat lightPos0[] = { 4.0f, 0.0f, 8.0f, 1.0f };
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
-    //Add directed light
+   
     GLfloat lightColor1[] = { 0.5f, 0.2f, 0.2f, 1.0f };
-    //Coming from the direction (-1, 0.5, 0.5)
+    
     GLfloat lightPos1[] = { -1.0f, 0.5f, 0.5f, 0.0f };
     glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
     glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
 }
-//initialize the first configurations
+
 void Initialize(void)
 {
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0.4f, 0.8f, 1.0f, 1.0f); //Change the background to sky blue
+    glClearColor(0.4f, 0.8f, 1.0f, 1.0f); 
     if (EnableLight) {
-        glEnable(GL_COLOR_MATERIAL); //Enable color
-        glEnable(GL_LIGHTING); //Enable lighting
-        glEnable(GL_LIGHT0); //Enable light #0
-        glEnable(GL_LIGHT1); //Enable light #1
-        glEnable(GL_NORMALIZE); //Automatically normalize normals
+        glEnable(GL_COLOR_MATERIAL); 
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0); 
+        glEnable(GL_LIGHT1); 
+        glEnable(GL_NORMALIZE); 
     }
 }
-//Configure window resize
+
 void resize(int w, int h)
 {
     glViewport(0, 0, w, h);
@@ -82,17 +83,23 @@ void resize(int w, int h)
     glLoadIdentity();
     gluPerspective(45.0, (double)w / (double)h, 1, 800.0);
 }
-void Write(char* string) {//Write string on the screen
+
+
+void Write(char* string) {
     while (*string)
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *string++);
 }
-//This Function will rotate the object according to the Angles
+
+
+
 void ManipulateViewAngle() {
-    glRotatef(view_rotx, 1.0, 0.0, 0.0);//Rotate Arround X axis
-    glRotatef(view_roty, 0.0, 1.0, 0.0);//Rotate Arround Y axis
-    glRotatef(view_rotz, 0.0, 0.0, 1.0);//Rotate Arround Z axis
+    glRotatef(view_rotx, 1.0, 0.0, 0.0);
+    glRotatef(view_roty, 0.0, 1.0, 0.0);
+    glRotatef(view_rotz, 0.0, 0.0, 1.0);
 }
-//This Function will reset the snake size and location...
+
+
+
 void Reset() {
     _x = 5;
     _z = 10;
@@ -106,14 +113,14 @@ void Reset() {
     view_rotz = 0.0F;
     headRotation = 90.0F;
 }
-//Display a welcome screen
+
 
 
 
 void WelcomeScreen() {
-    char tmp_str1[] = "=========WELCOME TO Azzonika Tutorials=========";
+    char tmp_str1[] = "=========WELCOME TO GAME!!!=========";
     char tmp_str2[] = "=========Simulation of Snake Game=========";
-    char tmp_str3[] = "=========To Start Playing please press 'S' or 's' =========";
+    char tmp_str3[] = "=========To Start Playing please press 'S' =========";
 
     glRasterPos2f(-40, 20);
     Write(tmp_str1);
@@ -124,22 +131,22 @@ void WelcomeScreen() {
 }
 void DrawSnake() {
     int  i;
-    //Drawing the head & the plane
+
     glPushMatrix();
     ManipulateViewAngle();
-    //This will draw the plane that the snake will run on.
+ 
     glPushMatrix();
-    //glColor3f(1, 0.7, 0.2);
+
     glColor3f(0.0, 0.6, 0.2);
     glTranslatef(75.0, -16.0, 75.0);
     glScalef(155, 5.0, 155);
     glutSolidCube(1);
     glPopMatrix();
-    //Here we will draw the Head of the snake
-    glColor3f(1, 0, 0);//Color it red
-    glTranslatef(_x, -10.0, _z);//Give it the location according to _x & _z
+
+    glColor3f(1, 0, 0);
+    glTranslatef(_x, -10.0, _z);
     glScalef(0.5, 0.5, 0.5);
-    glutSolidSphere(10, 20, 20);//Draw the head as a sphere a litle bit bigger than the body spheres
+    glutSolidSphere(10, 20, 20);
     glRotatef(headRotation, 0.0, 1.0, 0.0);
     glColor3f(1, 0, 0);
     glTranslatef(0, 0.0, 6.0);
@@ -153,12 +160,12 @@ void DrawSnake() {
     glutSolidSphere(10, 20, 20);
     glPopMatrix();
 
-    //Drawing the body
-    for (i = 0; i < size; i++) {//Loop throw the size and draw spheres representing the body
+
+    for (i = 0; i < size; i++) {
         glPushMatrix();
         ManipulateViewAngle();
-        glTranslatef(bodyPos[0][i], -10.0, bodyPos[1][i]);//this will locate the spheres
-        glColor3f(1, 0, 0);//Color Red
+        glTranslatef(bodyPos[0][i], -10.0, bodyPos[1][i]);
+        glColor3f(1, 0, 0);
         glScalef(0.5, 0.5, 0.5);
         glutSolidSphere(7, 20, 20);
         glPopMatrix();
@@ -166,7 +173,7 @@ void DrawSnake() {
 }
 void DrawFood()
 {
-    //Draw the Sphere representing the Food for the snake
+
     glPushMatrix();
     ManipulateViewAngle();
     glTranslatef(_bx, -10.0, _bz);
@@ -181,18 +188,18 @@ void GameStatus() {
     glRasterPos2f(5, 20);
     sprintf(tmp_str, "Snake Game");
     Write(tmp_str);
-    // Print the status of the game on the screen
+
     glColor3f(0, 0, 0);
     glRasterPos2f(5, 10);
     sprintf(tmp_str, "Level: %d Points: %d", lvl, points);
     Write(tmp_str);
 }
-//Generates Random Numbers for the location of the food that the snake will eat
+
 int RandomNumber(int high, int low)
 {
     return (rand() % (high - low)) + low;
 }
-//Generate the New Food that the snake will eat
+
 void newFood() {
     time_t seconds;
     time(&seconds);
@@ -200,21 +207,16 @@ void newFood() {
     _bx = RandomNumber(_Gfw - _Giw, _Giw + 10);
     _bz = RandomNumber(_Gfh - _Gih, _Gih + 10);
 }
-//This Function Will Check for Collision
+
 bool collision() {
     int i;
     for (i = 0; i < size; i++) {
         if ((bodyPos[0][i] == _x && bodyPos[1][i] == _z) ||
-            //((bodyPos[0][i] >= _x) && (bodyPos[0][i] <= _x + 5) && (bodyPos[1][i] >= _z) && (bodyPos[1][i] <= _z + 5)) ||
-            //((bodyPos[0][i] <= _x) && (bodyPos[0][i] >= _x - 5) && (bodyPos[1][i] <= _z) && (bodyPos[1][i] >= _z - 5)) ||
-            //((bodyPos[0][i] <= _x) && (bodyPos[0][i] >= _x - 5) && (bodyPos[1][i] >= _z) && (bodyPos[1][i] <= _z + 5)) ||
             ((bodyPos[0][i] >= _x) && (bodyPos[0][i] <= _x + 5) && (bodyPos[1][i] <= _z) && (bodyPos[1][i] >= _z - 5)))
             return true;
     }
     return false;
 }
-//This Function will move the snake according to the directions
-//Taken from the Keyboard keys
 void Run(int value) {
     int i;
     _oldX[1] = _x;
@@ -223,28 +225,25 @@ void Run(int value) {
     case RIGHT:
         headRotation = 90;
         _x += 6;
-        if (_x > _Gfw - 2) _x = _Giw - 1;//This will check if the snake is going into the border so it will appear on the other side
+        if (_x > _Gfw - 2) _x = _Giw - 1;
         break;
     case LEFT:
         headRotation = -90;
         _x -= 6;
-        if (_x < 0) _x = _Gfw - 2;//This will check if the snake is going into the border so it will appear on the other side
+        if (_x < 0) _x = _Gfw - 2;
         break;
     case UP:
         headRotation = 0;
         _z += 6;
-        if (_z > _Gfh - 2) _z = _Gih - 1;//This will check if the snake is going into the border so it will appear on the other side
+        if (_z > _Gfh - 2) _z = _Gih - 1;
         break;
     case Down:
         headRotation = 180;
         _z -= 6;
-        if (_z < 2) _z = _Gfh - 2;//This will check if the snake is going into the border so it will appear on the other side
+        if (_z < 2) _z = _Gfh - 2;
         break;
     }
-    //Checks for Collisoin if yes Game Over
     if (collision()) gameOver = true;
-    //Checks if the snake ate the food (check the X and Y)
-    // If yes it will increase the points & the size of the snake & create a new food
     if ((_x == _bx && _z == _bz) ||
         ((_x >= _bx) && (_x <= _bx + 4) && (_z >= _bz) && (_z <= _bz + 4)) ||
         ((_x <= _bx) && (_x >= _bx - 4) && (_z <= _bz) && (_z >= _bz - 4)) ||
@@ -256,7 +255,7 @@ void Run(int value) {
         newFood();
     }
 
-    for (i = 0; i < size; i++) {// Save the positions of the body parts
+    for (i = 0; i < size; i++) {
         _oldX[0] = _oldX[1];
         _oldZ[0] = _oldZ[1];
         _oldX[1] = bodyPos[0][i];
@@ -265,19 +264,16 @@ void Run(int value) {
         bodyPos[1][i] = _oldZ[0];
     }
 
-    //Set the Timer
     glutTimerFunc(130, Run, 0);
 }
 
-void Display(void) {//Draw Function
-    // Clear screen
+void Display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     if (EnableLight) initLight();
 
     glTranslatef(-60.0, 40.0, zoom);
-    //Check if the value of the Flag "Game Over is not True it will continue the game"
     if (!gameOver) {
         GameStatus();
         DrawFood();
@@ -285,7 +281,6 @@ void Display(void) {//Draw Function
     }
     else
         WelcomeScreen();
-    // Updates the screen
     glutPostRedisplay();
     glutSwapBuffers();
 }
@@ -324,7 +319,6 @@ void keyboard(unsigned char key, int x, int y)
     case 'Û': Reset();
         glutPostRedisplay();
         break;
-        //ESC to Exit
     case 27:
         exit(0);
         break;
